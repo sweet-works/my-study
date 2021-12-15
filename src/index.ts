ebda9d035ad368857b7d8ec0aa2b23f629063199
WYJTang <643392718@qq.com>
2021-12-15 17:06:12 +0800

    hello world

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@
  * @Autor: yaojie
  * @Date: 2021-12-15 16:25:30
  * @LastEditors: yaojie
- * @LastEditTime: 2021-12-15 16:25:32
+ * @LastEditTime: 2021-12-15 17:02:11
  * @Description: 
  */
 let hello: string = 'hello typescript!'
+document.querySelectorAll('.app')[0].innerHTML = hello;
